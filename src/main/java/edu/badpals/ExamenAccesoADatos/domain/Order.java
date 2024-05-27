a1bad7a2c19788624379330cb1c8a55562921692
Victor Clase <victorsancheznogueira@gmail.com>
2024-05-27 09:10:37 +0200

    feat(Order.java): create mapping for Order

--- src/main/java/edu/badpals/ExamenAccesoADatos/domain/Order.java
+++ src/main/java/edu/badpals/ExamenAccesoADatos/domain/Order.java
@@ -1,4 +1,58 @@
 package edu.badpals.ExamenAccesoADatos.domain;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="t_orders")
 public class Order {
+
+    public Order(){}
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(name = "ord_id", unique = true)
+    String id = "";
+
+
+    @ManyToOne
+    @JoinColumn(name="ord_wizard")
+    Wizard wizard;
+
+
+    @OneToOne
+    @JoinColumn(name="ord_item")
+    MagicalItem item;
+
+
+
+    @Override
+    public String toString() {
+        return wizard + ", " + item;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Wizard getWizard() {
+        return wizard;
+    }
+
+    public void setWizard(Wizard wizard) {
+        this.wizard = wizard;
+    }
+
+    public MagicalItem getItem() {
+        return item;
+    }
+
+    public void setItem(MagicalItem item) {
+        this.item = item;
+    }
 }
+
